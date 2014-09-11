88489cdd0c3d6ff8b26c289bd04116adace5fb5d
Martin Griesser <kaffeesiader@hotmail.com>
2014-09-11 16:33:36 +0200

    Beta1

---
+++ code/kinect_script.lua
@@ -0,0 +1,16 @@
+if (simGetScriptExecutionCount()==0) then
+	depthCam=simGetObjectHandle('kinect_visionSensor')
+	depthView=simFloatingViewAdd(0.9,0.9,0.2,0.2,0)
+	simAdjustView(depthView,depthCam,64)
+
+	-- publish depth image
+	cmd=simros_strmcmd_get_depth_sensor_data
+	topic='/simulation/kinect1/sensoring/depth_image'
+	simExtROS_enablePublisher(topic,1,cmd,depthCam,-1,'')
+	-- publish rgba image
+	cmd=simros_strmcmd_get_vision_sensor_image
+	topic='/simulation/kinect1/sensoring/rgb_image'
+	simExtROS_enablePublisher(topic,1,cmd,depthCam,-1,'')
+end
+
+simHandleChildScript(sim_handle_all_except_explicit)
