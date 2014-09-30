5cf54882a16672a1a0302ceaa9afcdd7d095bc45
Martin Griesser <kaffeesiader@hotmail.com>
2014-09-30 11:44:59 +0200

    Commit

--- code/kinect_script.lua
+++ code/kinect_script.lua
@@ -6,10 +6,12 @@ if (simGetScriptExecutionCount()==0) then
 	-- publish depth image
 	cmd=simros_strmcmd_get_depth_sensor_data
 	topic='/simulation/kinect1/sensoring/depth_image'
+	-- uncomment the following line to dissable the depth image publisher
 	simExtROS_enablePublisher(topic,1,cmd,depthCam,-1,'')
 	-- publish rgba image
 	cmd=simros_strmcmd_get_vision_sensor_image
 	topic='/simulation/kinect1/sensoring/rgb_image'
+	-- uncomment the following line to dissable the rgb image publisher
 	simExtROS_enablePublisher(topic,1,cmd,depthCam,-1,'')
 end
 
